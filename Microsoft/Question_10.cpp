b274dbbe5eed3b50a289631e77e4f14449cc99ce
pjyotianwar <70092582+pjyotianwar@users.noreply.github.com>
2023-01-16 17:57:28 +0530

    Solved

--- Microsoft/Question_10.cpp
+++ Microsoft/Question_10.cpp
@@ -1,2 +1,32 @@
 // Shortest Unsorted Continuous Subarray
 
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+        if(nums.size() < 2) return 0;
+        int res = 0,start= 0,end = 0;
+        vector<int> temp = nums;
+        sort(temp.begin(),temp.end());
+        for(int i = 0 ; i < nums.size(); i++)
+        {
+            if(nums[i] != temp[i])
+            {
+                start = i;
+                break;
+            }
+        }
+        for(int i = nums.size()-1 ; i >= 0 ; i--)
+        {
+        
+            if(nums[i] != temp[i])
+            {
+                end = i;
+                break;
+            }
+        }
+        if(end < start || end  == start || start < 0) 
+            return 0;
+        res = end -start + 1;
+        return res;
+    }
+};
--- Microsoft/Question_12.cpp
+++ Microsoft/Question_12.cpp
@@ -1,2 +1,26 @@
 // Longest Happy Prefix
 
+class Solution {
+public:
+    string longestPrefix(string s) {
+        vector<int> dp(s.size());
+        dp[0] = -1;
+        for(int i = 1;i<s.size();i++){
+            int j = dp[i-1] + 1;
+            while(s[i] != s[j]) {
+                if(j == 0){
+                    break;
+                }
+                j = dp[j-1] + 1;
+            }
+            if(s[i] == s[j]){
+                dp[i] = j;
+            } else {
+                dp[i] = -1;
+            }
+        }
+        int l = dp.back();
+        return s.substr(0,dp.back()+1);
+
+    }
+};
