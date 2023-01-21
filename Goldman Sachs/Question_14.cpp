bcf9c5bb73a5fb62bed8969033cbb5a0149f66c3
pjyotianwar <70092582+pjyotianwar@users.noreply.github.com>
2023-01-21 17:22:27 +0530

    Solved

--- Goldman Sachs/Question_14.cpp
+++ Goldman Sachs/Question_14.cpp
@@ -1,2 +1,21 @@
 // Count Nice Pairs in an Array
 
+class Solution {
+public:
+    int countNicePairs(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int> umap;
+        int res = 0;
+        for(int i = 0 ; i < n ; ++i){
+            int val = nums[i];
+            int reversed_num = 0;
+            while(val>0){
+                reversed_num = reversed_num * 10 + val % 10;
+                val /= 10;
+            }            
+            res += umap[reversed_num - nums[i]]++ % 1000000007;
+            res %= 1000000007;
+        }      
+        return res;
+    }
+};
--- Goldman Sachs/Question_15.cpp
+++ Goldman Sachs/Question_15.cpp
@@ -1,2 +1,30 @@
 // Maximum Good People Based on Statements
 
+class Solution {
+    bool valid(vector<vector<int>>& S, string& cur) {
+        for(int i = 0; i < n; i++) 
+            if(cur[i] == '1') 
+                for(int j = 0; j < n; j++) 
+                    if(S[i][j] != 2 && S[i][j] != cur[j] - '0') return false;
+        return true;
+    }
+public:
+    int n, ans = 0;
+    int maximumGood(vector<vector<int>>& S) {
+        n = size(S);
+        string cur = ""; cur.reserve(n);
+        dfs(S, cur, 0, 0);
+        return ans;
+    }
+    void dfs(vector<vector<int>>& S, string& cur, int i, int cnt) {
+        if(i == n) {
+            if(valid(S, cur)) ans = max(ans, cnt);
+            return;
+        }
+        cur.append(1, '0');
+        dfs(S, cur, i+1, cnt);
+        cur.back() = '1';
+        dfs(S, cur, i+1, cnt + 1);
+        cur.pop_back();        
+    }
+};
